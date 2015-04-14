028011a6571bf4804716fdedf2aae9b437c32ba9
진욱 <@172.17.0.113>
2015-04-14 15:12:45 +0900

    차2

---
+++ JavaPractice/src/org/dimigo/oop/Car2.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *    ㅣ_Car2
+ *
+ * 1. 개요:Car클래스 구현
+ * 2. 작성일 : 2015. 4. 13.
+ * </pre>
+ * 
+ * @author		: 김진욱
+ * @version		: 1.0
+ */
+public class Car2 {
+	private String company;
+	private String model;
+	private String color;
+	private int maxSpeed;
+	private int price;
+	
+	public Car2(){}
+	
+	public Car2(String newCompany, String newModel, String newColor, int newMaxSpeed, int newPrice)
+	{
+		company = newCompany;
+		model = newModel;
+		color = newColor;
+		maxSpeed = newMaxSpeed;
+		price = newPrice;
+	}
+	
+	public String getCompany()
+	{
+		return company;
+	}
+	
+	public String getModel()
+	{
+		return model;
+	}
+	
+	public String getColor()
+	{
+		return color;
+	}
+	
+	public int getMaxSpeed()
+	{
+		return maxSpeed;
+	}
+	
+	public int getPrice()
+	{
+		return price;
+	}
+	
+	
+}
---
+++ JavaPractice/src/org/dimigo/oop/CarTest2.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *    ㅣ_CarTest2
+ *
+ * 1. 개요:CarTest
+ * 2. 작성일 : 2015. 4. 13.
+ * </pre>
+ * 
+ * @author		: 김진욱
+ * @version		: 1.0
+ */
+public class CarTest2 {
+	public static void main(String[] args) {
+		Car2 modern = new Car2("현대자동차", "제네시스", "검정색", 225, 50000000);
+		Car2 Gia = new Car2("기아자동차", "K7", "흰색", 246, 40000000);
+		Car2 samsung = new Car2("삼성자동차", "SM7", "회색", 200, 38000000);
+		
+		System.out.println("<< 자동차 목록 >> ");
+		System.out.println("제조사명 : " + modern.getCompany());
+		System.out.println("모델명 : " + modern.getModel());
+		System.out.println("색상 : " + modern.getColor());
+		System.out.println("최대속도 : " + modern.getMaxSpeed() + "km");
+		System.out.println("가격 : " + String.format("%,d", modern.getPrice()) + "원");
+		System.out.println();
+		
+		System.out.println("제조사명 : " + Gia.getCompany());
+		System.out.println("모델명 : " + Gia.getModel());
+		System.out.println("색상 : " + Gia.getColor());
+		System.out.println("최대속도 : " + Gia.getMaxSpeed() + "km");
+		System.out.println("가격 : " + String.format("%,d", Gia.getPrice()) + "원");
+		System.out.println();
+		
+		System.out.println("제조사명 : " + samsung.getCompany());
+		System.out.println("모델명 : " + samsung.getModel());
+		System.out.println("색상 : " + samsung.getColor());
+		System.out.println("최대속도 : " + samsung.getMaxSpeed() + "km");
+		System.out.println("가격 : " + String.format("%,d", samsung.getPrice()) + "원");
+		System.out.println();
+	}
+
+}
